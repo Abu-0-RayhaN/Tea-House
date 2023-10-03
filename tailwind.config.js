ca34d3b699fe5776a20c1d3f2496b858907893a4
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-03 20:01:35 +0600

    clients section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-cup': "url('tea-house-resources/images/bg_cup.png')",
+      },
+      colors:{
+        'gradientOrange':'#ff8637',
+        'gradientRed':'#ff0301'
+      }
+    },
   },
+  variants: {},
   plugins: [],
-}
+};
