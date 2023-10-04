1014b43a340c940d0332e1a936f344e18962b4f4
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-04 22:20:07 +0600

    final commit from main branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
       },
       colors:{
         'gradientOrange':'#ff8637',
-        'gradientRed':'#ff0301'
+        'gradientRed':'#ff0301',
+        'footerbg':'#fff3eb',
+        'footerbg2':'#ffe6e6',
+        'tea-bg':'#ffe6e6',
+        'tea-bg2':'#fff2eb',
+        'tea-bg3':'#eef1eb',
+        'tea-bg4':'#dedede',
+        'tea-bg0':'#f9f9f9',
+
+      },
+      fontFamily:{
+        'manrope': ["Manrope", "sans-serif"],
       }
     },
   },
